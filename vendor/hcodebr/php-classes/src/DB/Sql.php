8d67ebae5b5ef488fd0e78ba0f13d422f62e2b5f
Francisco Valente <fraliva@gmail.com>
2018-05-24 21:39:15 -0300

    Cadastro de Produtos

--- vendor/hcodebr/php-classes/src/DB/Sql.php
+++ vendor/hcodebr/php-classes/src/DB/Sql.php
@@ -58,6 +58,9 @@ class Sql {
 
 		$this->setParams($stmt, $params);
 
+		//echo $rawQuery;
+		//var_dump($params);
+		//exit;
 		$stmt->execute();
 
 		return $stmt->fetchAll(\PDO::FETCH_ASSOC);
--- vendor/hcodebr/php-classes/src/Model/Product.php
+++ vendor/hcodebr/php-classes/src/Model/Product.php
@@ -23,13 +23,13 @@ class Product extends Model
 
 		$sql = new Sql();
 
-		$results = $sql->select("CALL sp_products_save(:idproduct, :desproduct, :vlprice, :vlwidth, :vlheight, :vllenght, :vlweight, :desurl)", array(
+		$results = $sql->select("CALL sp_products_save(:idproduct, :desproduct, :vlprice, :vlwidth, :vlheight, :vllength, :vlweight, :desurl)", array(
 			":idproduct"	=>$this->getidproduct(),
 			":desproduct"	=>$this->getdesproduct(),
 			":vlprice"		=>$this->getvlprice(),
 			":vlwidth"		=>$this->getvlwidth(),
 			":vlheight"		=>$this->getvlheight(),
-			":vllenght"		=>$this->getvllenght(),
+			":vllength"		=>$this->getvllength(),
 			":vlweight"		=>$this->getvlweight(),
 			":desurl"		=>$this->getdesurl()
 			));
@@ -61,6 +61,75 @@ class Product extends Model
 		));
 
 	} // End function delete
+
+	public function checkPhoto()
+	{
+
+		if (file_exists(
+			$_SERVER["DOCUMENT_ROOT"] . DIRECTORY_SEPARATOR .
+			"res". DIRECTORY_SEPARATOR .
+			"site". DIRECTORY_SEPARATOR .
+			"img". DIRECTORY_SEPARATOR .
+			"products". DIRECTORY_SEPARATOR .
+			$this->getidproduct(). ".jpg"
+		)) {
+
+			$url = "/res/site/img/products/". $this->getidproduct() . ".jpg";
+
+		} else {
+
+			$url = "/res/site/img/products/product.jpg";
+		}
+
+		return $this->setdesphoto($url);
+
+	}
+
+	public function getValues()
+	{
+
+		$this->checkPhoto();
+
+		$values = parent::getValues();
+
+		return $values;
+		
+	} // End function checkPhoto
+
+	public function setPhoto($file)
+	{
+
+		$extension = explode(".", $file["name"]);
+		$etenssion = end($extension);
+
+		switch ($extension){
+
+			case "jpg":
+			case "jpeg":
+				$image = imagecreatefromjpeg($file["tmp_name"]);
+				break;
+			case "gif":
+				$image = imagecreatefromgif($file["tmp_name"]);
+				break;
+			case "png":
+				$image = imagecreatefrompng($file["tmp_name"]);
+				break;
+		}
+
+		$dist = $_SERVER["DOCUMENT_ROOT"] . DIRECTORY_SEPARATOR .
+			"res". DIRECTORY_SEPARATOR .
+			"site". DIRECTORY_SEPARATOR .
+			"img". DIRECTORY_SEPARATOR .
+			"products". DIRECTORY_SEPARATOR .
+			$this->getidproduct(). ".jpg";
+
+		imagejpeg($image, $dist);
+
+		imagedestroy($image);
+
+		$this->checkPhoto();
+
+	} // End function setPhoto
 
 } // End class User
 
