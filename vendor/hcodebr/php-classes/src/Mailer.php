cbdf571913ca3edb0109dbee7346291b0f179866
Francisco Valente <fraliva@gmail.com>
2018-06-08 15:06:03 -0300

    Update Mailer.php

--- vendor/hcodebr/php-classes/src/Mailer.php
+++ vendor/hcodebr/php-classes/src/Mailer.php
@@ -9,7 +9,7 @@ class Mailer
 {
 
 	const USERNAME = "fraliva@gmail.com";
-	const PASSWORD = "Felv210470@GM";
+	const PASSWORD = "<?password?>";
 	const NAME_FROM = "Francisco Valente";
 
 	private $mail;
