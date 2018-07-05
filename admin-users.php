7927a5f0ed250a766d64a1a137ec0a3363b4f19f
Francisco Valente <fraliva@gmail.com>
2018-07-05 20:43:01 -0300

    Admin - Usuario ajustes

--- admin-users.php
+++ admin-users.php
@@ -3,6 +3,147 @@
 use \Hcode\PageAdmin;
 use \Hcode\Model\User;
 
+$app->get("/admin/users/:iduser/password", function($iduser){
+
+	User::verifyLogin();
+
+	$user = new User();
+
+	$user->get((int)$iduser);
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users-password", array(
+		"user"=>$user->getValues(),
+		"msgError"=>User::getError(),
+		"msgSuccess"=>User::getSuccess()
+	));
+
+});
+
+$app->post("/admin/users/:iduser/password", function($iduser){
+
+	User::verifyLogin();
+
+	if (!isset($_POST["despassword"]) || $_POST["despassword"] === "" ) {
+
+		User::setError("Preencha a nova senha");
+
+		header("Location: /admin/users/$iduser/password");
+		exit;
+
+	} 
+
+	if (!isset($_POST["despassword-confirm"]) || $_POST["despassword-confirm"] === "" ) {
+
+		User::setError("Preencha a confirmação da nova senha");
+
+		header("Location: /admin/users/$iduser/password");
+		exit;
+
+	} 
+
+	if ($_POST["despassword"] !== $_POST["despassword-confirm"] ) {
+
+		User::setError("As senhas informadas devem ser iguais.");
+
+		header("Location: /admin/users/$iduser/password");
+		exit;
+
+	} 
+
+	$user = new User();
+
+	$user->get((int)$iduser);
+
+	$user->setPassword(User::getPasswordHash($_POST["despassword"]));
+
+	User::setSuccess("Senha alterada com sucesso!");
+
+	header("Location: /admin/users/$iduser/password");
+	exit;
+
+});
+
+$app->get("/admin/users/create", function(){
+
+	User::verifyLogin();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users-create");
+
+});
+
+$app->post("/admin/users/create", function(){
+
+	User::verifyLogin();
+
+	$user = new User();
+
+	$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+
+	//$_POST['despassword'] = User::getPasswordHash($_POST['despassword']);
+ 
+	$user->setData($_POST);
+
+	$user->save();
+
+	header("Location: /admin/users");
+	exit;
+
+});
+
+$app->get("/admin/users/:iduser/delete", function($iduser){
+
+	User::verifyLogin();
+
+	$user = new User();
+
+	$user->get((int)$iduser);
+
+	$user->delete();
+
+	header("Location: /admin/users");
+	exit;
+
+});
+
+$app->get("/admin/users/:iduser", function($iduser){
+
+	User::verifyLogin();
+
+	$user = new User();
+
+	$user->get((int)$iduser);
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users-update", array(
+		"user"=>$user->getValues()
+	));
+
+});
+
+$app->post("/admin/users/:iduser", function($iduser){
+
+	User::verifyLogin();
+
+	$user = new User();
+
+	$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+
+	$user->get((int)$iduser);
+
+	$user->setData($_POST);
+
+	$user->update();
+
+	header("Location: /admin/users");
+	exit;
+
+});
+
 $app->get('/admin/users', function(){
 
 	User::verifyLogin();
@@ -44,84 +185,4 @@ $app->get('/admin/users', function(){
 	));
 
 });
-
-$app->get("/admin/users/create", function(){
-
-	User::verifyLogin();
-
-	$page = new PageAdmin();
-
-	$page->setTpl("users-create");
-
-});
-
-$app->get("/admin/users/:iduser/delete", function($iduser){
-
-	User::verifyLogin();
-
-	$user = new User();
-
-	$user->get((int)$iduser);
-
-	$user->delete();
-
-	header("Location: /admin/users");
-	exit;
-
-});
-
-$app->get("/admin/users/:iduser", function($iduser){
-
-	User::verifyLogin();
-
-	$user = new User();
-
-	$user->get((int)$iduser);
-
-	$page = new PageAdmin();
-
-	$page->setTpl("users-update", array(
-		"user"=>$user->getValues()
-	));
-
-});
-
-$app->post("/admin/users/create", function(){
-
-	User::verifyLogin();
-
-	$user = new User();
-
-	$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
-
-	//$_POST['despassword'] = User::getPasswordHash($_POST['despassword']);
- 
-	$user->setData($_POST);
-
-	$user->save();
-
-	header("Location: /admin/users");
-	exit;
-
-});
-
-$app->post("/admin/users/:iduser", function($iduser){
-
-	User::verifyLogin();
-
-	$user = new User();
-
-	$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
-
-	$user->get((int)$iduser);
-
-	$user->setData($_POST);
-
-	$user->update();
-
-	header("Location: /admin/users");
-	exit;
-
-});
-
 ?>
